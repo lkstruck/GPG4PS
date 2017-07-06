8b50a1ee65f3620a516e771b97aba650908e5162
coECC-Content07 <coECC-Content07@corp.target.com>
2017-07-06 16:36:11 -0500

    add ability to verify if GPG2 and Git are available

---
+++ validate-dependencies.ps1
@@ -0,0 +1,42 @@
+﻿
+
+#validate requisite installs
+#git
+function confirm-application{
+param(
+[string]$name
+)
+if ($application = gcm $name -errorAction SilentlyContinue)
+{
+return $application.Version
+}
+else
+{
+return 1
+}
+
+}
+
+
+
+
+
+
+
+
+
+if (($version = confirm-application -name git) -ne 1){
+write-host "Git $version is installed" 
+}
+else{
+write-host "Git is not installed please visit https://git-scm.com/download/win" 
+}
+
+if (($version = confirm-application -name gpg2) -ne 1){
+write-host "GPG2 is installed" 
+}
+else{
+write-host "GPG is not installed please visit https://www.gpg4win.org/download.html" 
+}
+
+
